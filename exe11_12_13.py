a5f49883fd97a96d76889f88a0085496b6a84e15
djs1193 <djs1193@yahoo.com>
2018-03-11 15:20:01 -0400

    Update exe11_12_13.py
    using python to know what a function does

--- exe11_12_13.py
+++ exe11_12_13.py
@@ -1,12 +1,14 @@
+
+# using python to know what a function does 
 print(abs.__doc__)
 
-
+# get the calendar for any month and year
 import calendar
 y = int(input("Input the year : "))
 m = int(input("Input the month : "))
 print(calendar.month(y, m))
 
-
+# how to print in multiple lines without using escape sequence 
 print("""
 a string that you "don't" have to escape
 This
